65a753696bf1d382479fc9c2c2bd680485c7e1b7
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-12-02 01:49:27 -0400

    modularized RNG, avoid panic, more randomness

--- src/_bool.rs
+++ src/_bool.rs
@@ -14,16 +14,31 @@ macro_rules! cap {
     };
 }
 
+/// # **BEST RNG EVER**
+/// Return value is in the interval 0 <= out < n.
+/// 
+/// # Panics
+/// Only if `n == 0`,
+/// because you can't have a number *equal AND not equal to 0*
+fn ultimate_rng(n: u128) -> u128 {
+    use std::time;
+    // guard clause, for speed
+    if n == 1 { return 0 };
+    match time::SystemTime::now().duration_since(time::UNIX_EPOCH) {
+        Ok(d) => d.as_nanos() % n,
+        Err(_) => 0
+    }
+}
+
+fn rand_cap() -> bool {
+    ultimate_rng(2) == 0
+}
+
 #[macro_export]
 /// Who needs certainty?
 macro_rules! maybe_bro {
     () => {
-        std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .expect("omegalul")
-            .as_micros()
-            % 2
-            == 0
+        rand_cap()
     };
 }
 
